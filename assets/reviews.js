037e4f4feb168e23c5808c64661f148ec22c0672
Siyu Liu <siyu.java.web@gmail.com>
2022-12-22 23:02:45 +1100

    polish code in js file and modify style in css file

--- assets/reviews.js
+++ assets/reviews.js
@@ -9,8 +9,7 @@ input.keyup(function () {
   if (movieName != "") {
     var availableNames = [];
     var movieIDUrl =
-      "https://api.themoviedb.org/3/search/movie?api_key=b3d061705cb162c0d2c4c93862143c72&query=" +
-      movieName;
+      "https://api.themoviedb.org/3/search/movie?api_key=b3d061705cb162c0d2c4c93862143c72&query=" +movieName;
     fetch(movieIDUrl)
       .then(function (response) {
         return response.json();
@@ -34,8 +33,7 @@ function getReviews() {
   content.innerHTML = "";
   var movieName = input.val();
   var movieIDUrl =
-    "https://api.themoviedb.org/3/search/movie?api_key=b3d061705cb162c0d2c4c93862143c72&query=" +
-    movieName;
+    "https://api.themoviedb.org/3/search/movie?api_key=b3d061705cb162c0d2c4c93862143c72&query=" +movieName;
   fetch(movieIDUrl)
     .then(function (response) {
       return response.json();
@@ -45,9 +43,7 @@ function getReviews() {
       var movieId = data.results[0].id;
       console.log(movieId);
       var reviewUrl =
-        "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/reviews?api_key=b3d061705cb162c0d2c4c93862143c72&language=en-US";
+        "https://api.themoviedb.org/3/movie/" + movieId + "/reviews?api_key=b3d061705cb162c0d2c4c93862143c72&language=en-US";
       console.log(reviewUrl);
       return reviewUrl;
     })
@@ -61,28 +57,18 @@ function getReviews() {
             var errorMessage = document.createElement("div");
             errorMessage.classList.add("error-message");
             content.appendChild(errorMessage);
-            errorMessage.textContent =
-              "Sorry, the review for this movie is not found";
+            errorMessage.textContent ="Sorry, the review for this movie is not found";
           } else {
             console.log(data);
             for (var i = 0; i < data.results.length; i++) {
               var card = document.createElement("div");
               var cardHeader = document.createElement("div");
               var cardBody = document.createElement("div");
-              cardHeader.textContent =
-                "By " +
-                data.results[i].author +
-                " posted at: " +
-                data.results[i].updated_at.split("T")[0];
+              cardHeader.textContent ="By "+data.results[i].author +" posted at: " +data.results[i].updated_at.split("T")[0];
               cardBody.textContent = data.results[i].content;
               card.setAttribute("class", "card");
               cardHeader.classList.add("card-header");
-              cardBody.classList.add(
-                "card-body",
-                "border-dark",
-                "bg-light",
-                "mb-3"
-              );
+              cardBody.classList.add("card-body","border-dark","bg-light","mb-3");
               content.appendChild(card);
               card.appendChild(cardHeader);
               card.appendChild(cardBody);
